86c92041136f45aba5086184b64828c9c249536e
Proless <proless8@gmail.com>
2021-01-10 21:23:31 +0100

    Changed the default behavior of the Get-Methods.

--- SettingsNet/Json/JsonSettingsSection.cs
+++ SettingsNet/Json/JsonSettingsSection.cs
@@ -3,7 +3,6 @@ using Newtonsoft.Json.Linq;
 using SettingsNet.Abstractions;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace SettingsNet.Json
@@ -15,12 +14,12 @@ namespace SettingsNet.Json
         private readonly JsonSerializer _jsonSerializer;
 
         /* Properties */
-        public JObject SettingsToken { get; }
-        public IEnumerable<ISettingsSection> Sections => GetSections();
+        public JObject Node { get; }
+        public IEnumerable<ISettingsSection> Sections => _settingsSections.Values;
         public string Key => PathHelpers.GetKey(Path);
-        public string Path => SettingsToken.Path;
-        public string Value => SettingsToken.ToString();
-        public int Count => SettingsToken.Children<JProperty>().Count(p => p.Value.Type != JTokenType.Object);
+        public string Path => Node.Path;
+        public string Value => Node.ToString();
+        public int Count => Node.Children<JProperty>().Count(p => p.Value.Type != JTokenType.Object);
 
         /* Indexer */
         public object this[string key]
@@ -37,10 +36,9 @@ namespace SettingsNet.Json
             _settingsSections = new Dictionary<string, ISettingsSection>();
             _jsonSerializer = jsonSerializer ?? throw new ArgumentNullException(nameof(jsonSerializer));
 
-            SettingsToken = token ?? throw new ArgumentNullException(nameof(token));
+            Node = token ?? throw new ArgumentNullException(nameof(token));
 
-            // Populate sections.
-            GetSections();
+            PopulateSections();
         }
 
         /* Methods */
@@ -50,138 +48,122 @@ namespace SettingsNet.Json
 
             return _settingsSections.TryGetValue(key, out var existingSection) ? existingSection : InternalGetSection(key);
         }
-        public T GetSection<T>(string key) where T : class, new()
+        public T GetSection<T>(string key, T defaultValue = default) where T : class, new()
         {
             if (!(GetSection(key) is JsonSettingsSection section))
-                throw new InvalidOperationException(
-                    $"Unable to determine the type of the section with the path: {Path}.{key}");
+                return defaultValue;
 
-
-            if (section.SettingsToken.HasValues) return section.Get<T>();
-
+            if (section.Node.HasValues) return section.Get<T>();
 
             // At this point we know that this is an new empty section
             // we then serialize an object of the type argument
             // to json and store that json as the new section structure.
             _settingsSections.Remove(key);
-            SettingsToken.Remove(key);
+            Node.Remove(key);
 
-            var instance = Activator.CreateInstance<T>();
+            var instance = defaultValue ?? Activator.CreateInstance<T>();
             var newSectionObject = JObject.FromObject(instance, _jsonSerializer);
-            SettingsToken[key] = newSectionObject;
+            Node[key] = newSectionObject;
             _settingsSections[key] = new JsonSettingsSection(newSectionObject, _jsonSerializer);
             return instance;
         }
         public T Get<T>() where T : class, new()
         {
-            return SettingsToken.ToObject<T>(_jsonSerializer);
+            return Node.ToObject<T>(_jsonSerializer);
         }
-        public T Get<T>(string key)
+        public T Get<T>(string key, T defaultValue = default)
         {
-            if (!(InternalGet(key) is string str)) return default;
-            using (var stringReader = new StringReader(str))
-            using (var reader = new JsonTextReader(stringReader))
-            {
-                return _jsonSerializer.Deserialize<T>(reader);
-            }
+            if (!(InternalGet(key) is string)) return defaultValue;
+
+            var property = Node.Children<JProperty>().FirstOrDefault(p => p.Name == key);
+
+            return property != null ? property.Value.ToObject<T>(_jsonSerializer) : defaultValue;
         }
 
         /* Helpers */
-
-        #region Sections
-        private IEnumerable<ISettingsSection> GetSections()
+        private void PopulateSections()
         {
-            return SettingsToken.Children<JProperty>()
-                .Where(p => p.Value.Type == JTokenType.Object)
-                .Select(p => p.Name)
-                .Select(GetSection);
+            _settingsSections.Clear();
+
+            foreach (var property in Node.Children<JProperty>().Where(p => p.Value.Type == JTokenType.Object))
+            {
+                var section = (JObject)property.Value;
+                _settingsSections[property.Name] = new JsonSettingsSection(section, _jsonSerializer);
+            }
         }
         private JsonSettingsSection InternalGetSection(string key)
         {
-            var existingSection = SettingsToken.Children<JProperty>()
+            if (Node.Children<JProperty>().Any(p => p.Name == key && p.Value.Type != JTokenType.Object))
+            {
+                // We return null, because the key doesn't correspond to a sub-section but rather a setting value
+                return null;
+            }
+
+            var existingSection = Node.Children<JProperty>()
                 .FirstOrDefault(p => p.Value.Type == JTokenType.Object && p.Name == key);
 
             JsonSettingsSection newSection;
             if (existingSection is null)
             {
-                var newSectionObject = new JObject();
-                SettingsToken[key] = newSectionObject;
+                var newSectionObject = JObject.Parse("{}");
+                Node[key] = newSectionObject;
                 newSection = new JsonSettingsSection(newSectionObject, _jsonSerializer);
             }
             else
             {
-                newSection = new JsonSettingsSection(existingSection.Value as JObject, _jsonSerializer);
+                newSection = new JsonSettingsSection((JObject)existingSection.Value, _jsonSerializer);
             }
 
             _settingsSections[key] = newSection;
             return newSection;
         }
-        #endregion
 
-        #region Settings
         private void InternalSet(string key, object value)
         {
             if (key is null) throw new ArgumentNullException(nameof(key));
 
-            // update sections
-            GetSections();
-
+            // remove the setting if the value passed in is null,
+            // regardless whether it is a section or not.
             if (value is null)
             {
-                SettingsToken[key]?.Remove();
+                Node.Remove(key);
                 _settingsSections.Remove(key);
-                // update sections
-                GetSections();
                 return;
             }
 
-            var property = SettingsToken.Children<JProperty>().FirstOrDefault(p => p.Name == key);
-            var valueToken = JToken.FromObject(value, _jsonSerializer);
-
             // we remove the property regardless because we need to replace it
             // either with a section or a single setting value (primitive type)
-            property?.Remove();
+            Node.Children<JProperty>()
+               .FirstOrDefault(p => p.Name == key)?.Remove();
+
+            // parse the value that we want to set to a JToken to determine it's type
+            var valueToken = JToken.FromObject(value, _jsonSerializer);
 
 
-            // differentiate between JObject and other J-Types
+            // differentiate between JObject and other JValues
             if (valueToken.Type == JTokenType.Object)
             {
-                var newSection = new JsonSettingsSection(valueToken as JObject, _jsonSerializer);
+                var newSection = new JsonSettingsSection((JObject)valueToken, _jsonSerializer);
                 _settingsSections[key] = newSection;
             }
             else
             {
-                // we know this is a section and is being replaced with a single setting value
-                if (_settingsSections.ContainsKey(key))
-                {
-                    _settingsSections.Remove(key);
-                }
+                // make sure to remove the value if it was a section,
+                // because we replaced it with this JValue
+                _settingsSections.Remove(key);
             }
 
-            SettingsToken[key] = valueToken;
+            // finally we only need to set the value.
+            Node[key] = valueToken;
         }
         private object InternalGet(string key)
         {
             if (key is null) throw new ArgumentNullException(nameof(key));
 
-            return SettingsToken.Children<JProperty>()
-                .Where(p => p.Value.Type != JTokenType.Object)
+            return Node.Children<JProperty>()
                 .Where(p => p.Name == key)
                 .Select(p => p.Value.ToString())
                 .FirstOrDefault();
         }
-        private string Serialize(object value)
-        {
-            if (value is null) return null;
-
-            using (var stream = new StringWriter())
-            using (var writer = new JsonTextWriter(stream))
-            {
-                _jsonSerializer.Serialize(writer, value);
-                var json = stream.ToString();
-                return json;
-            }
-        }
-        #endregion
     }
 }
