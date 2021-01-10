a1be891ec2a0e124938f122bbd8771a98deed0f6
Proless <proless8@gmail.com>
2021-01-10 21:21:36 +0100

    Added a few testing methods.
    - Testing retrieving a value by a specified key and type.

--- SettingsNet.Test/SettingsSectionTests.cs
+++ SettingsNet.Test/SettingsSectionTests.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using Newtonsoft.Json;
+using SettingsNet.Abstractions;
 using SettingsNet.Json;
 using Xunit;
 
@@ -6,17 +8,52 @@ namespace SettingsNet.Test
 {
     public class SettingsSectionTests
     {
+        private readonly ISettingsSource _source;
+        private readonly ISettingsProvider _provider;
+        private readonly ISettingsSection _root;
+
+        public SettingsSectionTests()
+        {
+            _source = new JsonSettingsSource("Resources\\settings.json", new JsonSerializer());
+            _provider = _source.Load();
+            _root = _provider.GetRoot();
+        }
+
         [Fact]
-        public void Test01()
+        public void TestStringValue()
         {
             // Arrange
-            var source = new JsonSettingsSource("Resources\\settings.json", new JsonSerializer());
-            var provider = source.Load();
-            var root = provider.GetRoot();
 
             // Act
+            var name = _root.Get<string>("name");
 
             // Assert
+            Assert.Equal("Morrowind", name);
+        }
+
+        [Fact]
+        public void TestIntValue()
+        {
+            // Arrange
+
+            // Act
+            var downloads = _root.Get<int>("downloads",56);
+
+            // Assert
+            Assert.Equal(27161472, downloads);
+        }
+
+        [Fact]
+        public void TestUriValue()
+        {
+            // Arrange
+
+            // Act
+            var url = _root.Get<Uri>("forum_url");
+            var expected = new Uri("https://forums.nexusmods.com/index.php?/forum/111-morrowind/");
+
+            // Assert
+            Assert.Equal(expected, url);
         }
     }
 }
